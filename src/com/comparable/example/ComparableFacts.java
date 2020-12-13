ed00b99b61e9b3a3cd9eef03b237edb1275610d1
Amit K <jdamit2027@gmail.com>
2020-12-13 20:13:08 +0530

    second commit Demo example StringBuffer and StringBuilder are Comparable from java 11.

--- src/com/comparable/example/ComparableFacts.java
+++ src/com/comparable/example/ComparableFacts.java
@@ -5,39 +5,44 @@ import java.util.TreeSet;
 public class ComparableFacts {
 
 	public static void main(String[] args) {
-		TreeSet<Integer> integerSet1 = new TreeSet<Integer>((o1,o2)->o1.compareTo(o2));
+		TreeSet<Integer> integerSet1 = new TreeSet<Integer>((o1, o2) -> o1.compareTo(o2));
 		integerSet1.add(30);
 		integerSet1.add(10);
 		integerSet1.add(40);
 		integerSet1.add(05);
-		integerSet1.forEach(item->System.out.println(item));
-		
+		integerSet1.forEach(item -> System.out.println(item));
+
 		System.out.println("----------------------------------------------");
-		TreeSet<Integer> integerSet = new TreeSet<Integer>(
-				(o1,o2)->{
-					Integer i1 = (Integer)o1;
-					Integer i2 = (Integer)o2;
-					if(i1<i2)return +1;
-					else if(i1>i2) return -1;
-					else return 0;
-				}
-				);
+		TreeSet<Integer> integerSet = new TreeSet<Integer>((o1, o2) -> {
+			Integer i1 = (Integer) o1;
+			Integer i2 = (Integer) o2;
+			if (i1 < i2)
+				return +1;
+			else if (i1 > i2)
+				return -1;
+			else
+				return 0;
+		});
 		integerSet.add(30);
 		integerSet.add(10);
 		integerSet.add(40);
 		integerSet.add(05);
-		//integerSet.forEach(item->System.out.println(item));
-		
-		TreeSet<Integer> integerSet2 = new TreeSet<Integer>((o1,o2)->+1);
+		integerSet.forEach(item -> System.out.println(item));
+
+		System.out.println("----------------------------------------------");
+
+		TreeSet<Integer> integerSet2 = new TreeSet<Integer>((o1, o2) -> +1);
 		integerSet2.add(12);
 		integerSet2.add(10);
 		integerSet2.add(40);
 		integerSet2.add(40);
 		integerSet2.add(0);
 		integerSet2.add(23);
-		integerSet2.forEach(item->System.out.println(item));
-		
-		TreeSet<Integer> integerSet3 = new TreeSet<Integer>((o1,o2)->-1);
+		integerSet2.forEach(item -> System.out.println(item));
+
+		System.out.println("----------------------------------------------");
+
+		TreeSet<Integer> integerSet3 = new TreeSet<Integer>((o1, o2) -> -1);
 		integerSet3.add(12);
 		integerSet3.add(10);
 		integerSet3.add(40);
@@ -45,9 +50,23 @@ public class ComparableFacts {
 		integerSet3.add(0);
 		integerSet3.add(23);
 		System.out.println();
-		integerSet3.forEach(item->System.out.println(item));
-		
-		TreeSet<String> stringSet = new TreeSet<String>((o1,o2)->o2.compareTo(o1));
+		integerSet3.forEach(item -> System.out.println(item));
+
+		System.out.println("----------------------------------------------");
+
+		TreeSet<Integer> integerSet4 = new TreeSet<Integer>((o1, o2) -> 0);
+		integerSet4.add(12);
+		integerSet4.add(10);
+		integerSet4.add(40);
+		integerSet4.add(40);
+		integerSet4.add(0);
+		integerSet4.add(23);
+		System.out.println();
+		integerSet4.forEach(item -> System.out.println(item));
+
+		System.out.println("----------------------------------------------");
+
+		TreeSet<String> stringSet = new TreeSet<String>((o1, o2) -> o2.compareTo(o1));
 		stringSet.add("z");
 		stringSet.add("a");
 		stringSet.add("x");
@@ -55,9 +74,11 @@ public class ComparableFacts {
 		stringSet.add("k");
 		stringSet.add("n");
 		System.out.println();
-		stringSet.forEach(item->System.out.println(item));
-		
-		TreeSet<Object> stringBufferSet = new TreeSet<Object>((o1,o2)->o1.toString().compareTo(o2.toString()));
+		stringSet.forEach(item -> System.out.println(item));
+
+		System.out.println("----------------------------------------------");
+
+		TreeSet<Object> stringBufferSet = new TreeSet<Object>((o1, o2) -> o1.toString().compareTo(o2.toString()));
 		stringBufferSet.add(new StringBuffer("z"));
 		stringBufferSet.add(new StringBuffer("aa"));
 		stringBufferSet.add(new StringBuffer("x"));
@@ -65,10 +86,15 @@ public class ComparableFacts {
 		stringBufferSet.add(new StringBuffer("k"));
 		stringBufferSet.add(new StringBuffer("n"));
 		stringBufferSet.add(new String("bb"));
+		stringBufferSet.add(new String("bac"));
+		stringBufferSet.add(new String("bbC"));
+		stringBufferSet.add(new String("bB"));
 		System.out.println();
-		stringBufferSet.forEach(item->System.out.println(item));
-		
-		TreeSet<StringBuffer> onlyStringBufferset = new TreeSet<>((o1,o2)->o1.toString().compareTo(o2.toString()));
+		stringBufferSet.forEach(item -> System.out.println(item));
+		System.out.println("----------------------------------------------");
+		System.out.println("-----------------Before Java 11---------------");
+		//This code works fine in both java 8 and java 11.
+		TreeSet<StringBuffer> onlyStringBufferset = new TreeSet<>((o1, o2) -> o1.toString().compareTo(o2.toString()));
 		onlyStringBufferset.add(new StringBuffer("z"));
 		onlyStringBufferset.add(new StringBuffer("aa"));
 		onlyStringBufferset.add(new StringBuffer("x"));
@@ -76,7 +102,20 @@ public class ComparableFacts {
 		onlyStringBufferset.add(new StringBuffer("k"));
 		onlyStringBufferset.add(new StringBuffer("n"));
 		System.out.println("onlyStringBufferset");
-		onlyStringBufferset.forEach(item->System.out.println(item));
+		onlyStringBufferset.forEach(item -> System.out.println(item));
+		
+		System.out.println("----------------------------------------------");
+		System.out.println("-----------------From Java 11-----------------");
+		//This code only works fine in Java 11 but it will give ClassCastException
+		TreeSet<StringBuffer> onlyStringBuffersetJava11 = new TreeSet<>();
+		onlyStringBuffersetJava11.add(new StringBuffer("z"));
+		onlyStringBuffersetJava11.add(new StringBuffer("aa"));
+		onlyStringBuffersetJava11.add(new StringBuffer("x"));
+		onlyStringBuffersetJava11.add(new StringBuffer("m"));
+		onlyStringBuffersetJava11.add(new StringBuffer("k"));
+		onlyStringBuffersetJava11.add(new StringBuffer("n"));
+		System.out.println("onlyStringBufferset");
+		onlyStringBufferset.forEach(item -> System.out.println(item));
 	}
 
 }
--- src/com/comparable/example/cyient/ComparatorCyient.java
+++ src/com/comparable/example/cyient/ComparatorCyient.java
@@ -9,10 +9,10 @@ public class ComparatorCyient {
 	public static void main(String[] args) {
 		
 		List<String> list = new ArrayList<String>();
-		list.add("Two");
-		list.add("One");
-		list.add("Three");
-		list.add("Four");
+		list.add("Two"); //w
+		list.add("One"); //n
+		list.add("Three"); //h
+		list.add("Four"); //o
 		
 		Collections.sort(list, (o1, o2)-> o1.charAt(1) - o2.charAt(1));
 		System.out.println(list);
